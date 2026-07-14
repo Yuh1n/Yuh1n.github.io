9d9b992401e99bbdfe368605b3b5404adc6c47a2
Yuh1n <1667729346@qq.com>
2026-07-14 18:52:25 +0800

    Use Pixiv artwork as blog background

--- src/config.ts
+++ src/config.ts
@@ -17,12 +17,12 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "assets/images/home-background.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "assets/images/pixiv-111024784-4.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "Pixiv #111024784", // Credit text to be displayed
+			url: "https://www.pixiv.net/artworks/111024784", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
